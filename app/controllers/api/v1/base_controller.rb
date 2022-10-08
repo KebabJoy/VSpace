208da4e19907b04b310cf43f431d8badb96998dc
Nikas <niks@level.travel>
2022-10-08 21:28:08 +0300

    try fix heroku

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class BaseController < ApplicationController
-      protect_from_forgery with: :null_session
+      skip_before_action :verify_authenticity_token
 
       before_action :authorize!
 
